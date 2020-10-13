13ca7752c53d9cb8c5473290bf0819213a68d242
dbrisco93 <dbrisco93@gmail.com>
2020-10-13 03:04:26 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+    attr_accessor :author
+    attr_reader :title
+
+    def title=(title)
+        @title = title
+    end
+    
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+    attr_accessor :artist
+    attr_reader :title
+
+    def title=(title)
+        @title = title
+    end
+
+end
